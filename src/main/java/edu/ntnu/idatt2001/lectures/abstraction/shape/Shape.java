68aa5fd49800e7dbad7a64558e20e97509f838da
majidrouhanintnu <42731301+majidrouhanintnu@users.noreply.github.com>
2023-01-30 13:30:53 +0100

    upd

---
+++ src/main/java/edu/ntnu/idatt2001/lectures/abstraction/shape/Shape.java
@@ -0,0 +1,21 @@
+
+package edu.ntnu.idatt2001.lectures.abstraction.shape;
+
+/**
+ * Abstract class for shapes.
+ *
+ * @author rouhani
+ *
+ */
+public abstract class Shape {
+
+  /**
+   * Calculates the area of a shape.
+   *
+   * @return double
+   */
+  public abstract double calcArea();
+
+  public abstract double calcCircumference();
+
+}
